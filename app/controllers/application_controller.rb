e533f8908bef90f6d5a418afa2b94265746befb5
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-12 03:22:40 +0000

    1st commit - forex finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+  def homepage
+    render({:template => "home.html.erb"})
+  end
+  
+  def forex_subhome
+    @raw_forex = open("https://api.exchangerate.host/symbols").read
+    @parsed_forex = JSON.parse(@raw_forex)
+    @symbol_hash = @parsed_forex.fetch("symbols")
+    @array_symbol = @symbol_hash.keys
+    render({:template => "forex_subhome.html.erb"})
+  end 
+  def forex_2ndlayer
+    @symbol = params.fetch("the_symbol")
+    @raw_forex = open("https://api.exchangerate.host/symbols").read
+    @parsed_forex = JSON.parse(@raw_forex)
+    @symbol_hash = @parsed_forex.fetch("symbols")
+    @array_symbol = @symbol_hash.keys
+    render({:template => "forex_2ndlayer.html.erb"})
+  end
+  
+  def forex_3rdlayer
+    @from_currency = params.fetch("from")
+    @to_currency = params.fetch("to")
+    @raw_exchange = open("https://api.exchangerate.host/convert?from=#{@from_currency}&to=#{@to_currency}").read
+    @parsed_exchange = JSON.parse(@raw_exchange)
+    @rate = @parsed_exchange.fetch("result")
+
+    render({:template => "forex_3rdlayer.html.erb"})
+  end
+  
+  def covid_subhome
+    render({:template => "covid_subhome.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/",{:controller => "application", :action => "homepage"})
+  get("/forex",{:controller => "application", :action =>"forex_subhome"})
+  get("/covid",{:controller => "application", :action =>"covid_subhome"})
+  get("/forex/:the_symbol",{:controller => "application", :action =>"forex_2ndlayer"})
+  get("/forex/:from/:to",{:controller => "application", :action =>"forex_3rdlayer"})
+  # get("",{:controller => "application", :action =>""})
 end
